58aadad93e142174dbe20fb47d2155234748dcb2
Ekaterina Yakutova <84589010+RAKETIK@users.noreply.github.com>
2021-05-29 15:53:36 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Осу
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 25;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("Magneto", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.Location = new System.Drawing.Point(710, 32);
+            this.scoreLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(31, 28);
+            this.scoreLabel.TabIndex = 0;
+            this.scoreLabel.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(5F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(827, 473);
+            this.Controls.Add(this.scoreLabel);
+            this.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label scoreLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Осу
+{
+    public partial class Form1 : Form
+    {
+        public Bitmap HandlerTexure = Resource1.Handler, //Определяем два поля с кругами
+                      TargetTexure = Resource1.Target;
+        private Point _targetPosition = new Point(300, 300); //Описывает позиция точки, в которую мы должны попадать
+        private Point _direction = Point.Empty; //Направление
+        private int _score = 0; //Поле в котором храниться текущее количество очков
+        public Form1()
+        {
+            InitializeComponent();
+
+            SetStyle(ControlStyles.OptimizedDoubleBuffer |
+                ControlStyles.AllPaintingInWmPaint |
+                ControlStyles.UserPaint, true); //Артибуты, позволяют избавиться от мерцания
+
+            UpdateStyles(); //Чтобы все стили применились
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Refresh(); //Перерисовывает всю форму и вызывает Form1_Paint
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            timer2.Interval = r.Next(25, 1000); //Случайный интервал
+            _direction.X = r.Next(-1, 2); //Каждый тик этого таймера _direction.X изменяет на случайное число
+            _direction.Y = r.Next(-1, 2); //Каждый тик этого таймера _direction.Y изменяет на случайное число
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics; //Графический контекст
+
+            var localPosition = this.PointToClient(Cursor.Position); //Точка, которая бегает за курсором
+
+            _targetPosition.X += _direction.X * 10; //Смещение направление точки по оси X со скоростью умноженой на 10
+            _targetPosition.Y += _direction.Y * 10; //Смещение направление точки по оси Y со скоростью умноженой на 10
+
+            if (_targetPosition.X < 0 || _targetPosition.X > 500) //Если выходит за границы по оси X, то
+            {
+                _direction.X *= -1; //Домножает направление по X (разворачиваем)
+            }
+            if (_targetPosition.Y < 0 || _targetPosition.Y > 500) //Если выходит за границы по оси Y, то
+            {
+                _direction.Y *= -1; //Домножает направление по Y (разворачиваем)
+            }
+            
+            //Воспользуемся теоремой Пифагора. Из одной позиции вычесть другую, соотвественно найти гипотенузу - Это будет расстояние между точками
+            Point between = new Point(localPosition.X - _targetPosition.X, localPosition.Y - _targetPosition.Y);
+            float distance = (float)Math.Sqrt((between.X * between.X) + (between.Y * between.Y)); //Сумма квадратов катетов
+
+            if (distance < 20) //Если дистанция меньше 20 пикселей, то очки начисляются
+
+            {
+                AddScore(1);
+            }    
+
+            var handlerRect = new Rectangle(localPosition.X - 50, localPosition.Y - 50, 100, 100);
+            var targetRect = new Rectangle(_targetPosition.X - 50, _targetPosition.Y - 50, 100, 100);
+
+            g.DrawImage(HandlerTexure, handlerRect); //Метод, который рисует картинку
+            g.DrawImage(TargetTexure, targetRect); //Метод, который рисует картинку
+
+        }
+
+        private void AddScore(int score) //Метод, который добавляет очки
+
+        {
+            _score += score; //К полю прибавлем значение параметра score
+            scoreLabel.Text = _score.ToString(); //В поле Text можно дать текст, который будет выводиться
+        }
+    }
+}
